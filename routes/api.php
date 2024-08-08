eeb35abdcb3eb16bd6e672a13d3e0289fb4e8e90
VitorNuness <vitor1nuness@gmail.com>
2024-08-08 15:10:59 -0300

    adding auth protection to holiday plans crud routes

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,11 @@
 use App\Http\Controllers\HolidayPlan;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/plans', HolidayPlan\GetAll::class)->name('plans.index');
-Route::get('/plans/{id}', HolidayPlan\GetById::class)->name('plans.show');
-Route::post('/plans/new', HolidayPlan\Store::class)->name('plans.store');
-Route::put('/plans/{holidayPlan}/update', HolidayPlan\Update::class)->name('plans.update');
-Route::delete('/plans/{holidayPlan}/delete', HolidayPlan\Destroy::class)->name('plans.destroy');
-Route::get('/plans/{holidayPlan}/pdf', HolidayPlan\PdfDownload::class)->name('plans.pdf');
+Route::middleware('auth:api')->group(function () {
+    Route::get('/plans', HolidayPlan\GetAll::class)->name('plans.index');
+    Route::get('/plans/{id}', HolidayPlan\GetById::class)->name('plans.show');
+    Route::post('/plans/new', HolidayPlan\Store::class)->name('plans.store');
+    Route::put('/plans/{holidayPlan}/update', HolidayPlan\Update::class)->name('plans.update');
+    Route::delete('/plans/{holidayPlan}/delete', HolidayPlan\Destroy::class)->name('plans.destroy');
+    Route::get('/plans/{holidayPlan}/pdf', HolidayPlan\PdfDownload::class)->name('plans.pdf');
+});
