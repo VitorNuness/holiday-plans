b6a2fe537fa0b1a8f3164b5bd8517127a85fb126
VitorNuness <vitor1nuness@gmail.com>
2024-08-08 11:16:39 -0300

    adding factory and seeder for holiday plans

---
+++ database/factories/HolidayPlanFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\HolidayPlan>
+ */
+class HolidayPlanFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->text(50),
+            'description' => fake()->text(255),
+            'date' => fake()->date(),
+            'location' => fake()->text(100),
+            'participants' => null,
+        ];
+    }
+}
