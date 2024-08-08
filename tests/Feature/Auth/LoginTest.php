a7972ddd3a5da3b2ce0b86e327686e4490426989
VitorNuness <vitor1nuness@gmail.com>
2024-08-08 18:04:17 -0300

    testing should be user can login

---
+++ tests/Feature/Auth/LoginTest.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Models\User;
+
+use function Pest\Laravel\assertDatabaseHas;
+use function Pest\Laravel\postJson;
+
+it('should be able user to login', function () {
+    User::factory()->create([
+        'email' => 'john.doe@email.com',
+        'password' => 'password'
+    ]);
+
+    postJson(route('auth.login'), [
+        'email' => 'john.doe@email.com',
+        'password' => 'password'
+    ])->assertSuccessful()
+        ->assertJsonStructure(['token']);
+});
+
+it('should be require email to login', function () {
+    postJson(route('auth.login'), [
+        'email' => '',
+        'password' => 'password',
+    ])->assertInvalid([
+        'email' => 'required',
+    ]);
+});
+
+it('should be require password to login', function () {
+    postJson(route('auth.login'), [
+        'email' => 'john.doe@email.com',
+        'password' => '',
+    ])->assertInvalid([
+        'password' => 'required',
+    ]);
+});
+
+it('should be dont login with wrong credentials', function () {
+    User::factory()->create([
+        'email' => 'john.doe@email.com',
+        'password' => 'password'
+    ]);
+
+    postJson(route('auth.login'), [
+        'email' => 'john.doe@email.com',
+        'password' => 'aaaaaaa'
+    ])->assertUnauthorized();
+});
