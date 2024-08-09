c61f96f3c47275d3c5b036460b9b0d9e802f7499
VitorNuness <vitor1nuness@gmail.com>
2024-08-09 12:04:48 -0300

    refactor models holiday plan and user adding relation with then, and clear dead imports

--- app/Models/HolidayPlan.php
+++ app/Models/HolidayPlan.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class HolidayPlan extends Model
 {
@@ -17,5 +18,10 @@ class HolidayPlan extends Model
             'date' => 'date:Y-m-d',
             'participants' => 'array',
         ];
+    }
+
+    protected function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,49 +2,38 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Passport\HasApiTokens;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function holidayPlans(): HasMany
+    {
+        return $this->hasMany(HolidayPlan::class);
     }
 
     public function getJWTIdentifier()
