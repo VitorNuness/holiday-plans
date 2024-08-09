5db14c33e68a5f1ba5c9d61956b703fbfa9e9ae7
VitorNuness <vitor1nuness@gmail.com>
2024-08-09 10:19:20 -0300

    refactor adding list render for participants

--- resources/views/pdf/holiday-plan.blade.php
+++ resources/views/pdf/holiday-plan.blade.php
@@ -1,5 +1,23 @@
 <h1>{{ $data->title }}</h1>
 <p>{{ $data->description }}</p>
-<p>Date: {{ $data->date->format('Y-m-d') }}</p>
-<p>Location: {{ $data->location }}</p>
-<p>Participants: {{ $data->participants }}</p>
+
+<hr>
+<h3>Informations</h3>
+<p>
+    <b>Date:</b>
+    {{ $data->date->format('Y-m-d') }}
+</p>
+<p>
+    <b>Location:</b>
+    {{ $data->location }}
+</p>
+
+@if (count($data->participants) > 0)
+<hr>
+<h3>Participants</h3>
+<ul>
+    @foreach ($data->participants as $participant)
+    <li>{{ $participant }}</li>
+    @endforeach
+</ul>
+@endif
